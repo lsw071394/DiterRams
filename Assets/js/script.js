2084ec9b31e86ab676420fc8d96f5bea0ca7e620
lsw071394 <sangwon0730@gmail.com>
2021-02-02 17:14:36 +0900

    css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function (e) {
 $(function () {
     console.log('jQuery Load')
 
-    $('body').css({ 'touch-action': 'none', 'overflow': 'hidden', 'background-color': '#000' });
+    $('body').css({ 'touch-action': 'none', 'overflow': 'hidden', 'background-color': '#141414' });
 
     $(".ham").click(function (e) {
         e.preventDefault();
