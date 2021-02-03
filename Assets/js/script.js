a48f809ac4545b25adf9919b33a43c72f29aba37
lsw071394 <sangwon0730@gmail.com>
2021-02-03 17:52:36 +0900

    .css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,79 +1,27 @@
-
+// 모바일 페이지 로드시 상단 주소창 숨김
 window.addEventListener('load', function (e) {
 
     setTimeout(scrollTo, 0, 0, 1);
 }, false);
+//
+
 
 $(function () {
     console.log('jQuery Load')
 
+
+    // 광고창 사라지기전 body 리셋값
     $('body').css({ 'touch-action': 'none', 'overflow': 'hidden', 'background-color': '#141414' });
-
-    $(".ham").click(function (e) {
-        e.preventDefault();
-        $(this).toggleClass("active")
-    });
-
-    var mySwiper = new Swiper('div.gnb > .swiper-container', {
-        direction: 'horizontal',
-        loop: false,
-        slidesPerView: 'auto',
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
-    })
-
-    var goodSwiper = new Swiper('.card-area .swiper-container', {
-        direction: 'horizontal',
-        loop: false,
-        slidesPerView: 'auto',
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
-    })
-
-
-    $('.close-popUp').click(function (e) {
-        console.log('click');
-        e.preventDefault();
-        $('div').remove('.popUp');
-    })
-
-    $('a.side-remote').on('click', function (e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 600);
-    })
-
-
+    // 광고창 닫기 버튼
     $('.x').on('click', function () {
         console.log('click');
         $('.shinhan-ad').animate({ height: '-=100vh' }, 1500, 'easeInOutBack', function () {
             $('div').remove('.shinhan-ad');
             $('body').css({ 'padding-top': '100px', 'touch-action': 'auto', 'overflow': 'auto', 'background-color': '#fff' })
             $('header').css({ 'position': 'fixed' })
-
         });
-
     });
-
-    var sc = window.scrollY;
-
-
-
-    $(window).scroll(function () {
-        var sc = $(document).scrollTop();
-        console.log(sc);
-        if (sc < 210) {
-            $('.side-remote').hide(500);
-        } else { $('.side-remote').show(500) }
-    })
-
-
-
+    // 광고창 닫기 슬라이드 모션 (아래에서 위로)
     var startX, startY, endX, endY;
     $(".shinhan-ad").on('touchstart', function (event) {
         startX = event.originalEvent.changedTouches[0].screenX;
@@ -90,6 +38,56 @@ $(function () {
             });
         }
     });
+
+    // 햄버거 이벤트
+    $(".ham").click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass("active")
+    });
+
+    // 팝업창 닫기
+    $('.close-popUp').click(function (e) {
+        console.log('click');
+        e.preventDefault();
+        $('div').remove('.popUp');
+    })
+
+
+    // 메인 슬라이더
+    var mySwiper = new Swiper('div.gnb > .swiper-container', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 'auto',
+    })
+
+    // 카드 슬라이더
+    var goodSwiper = new Swiper('.card-area .swiper-container', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 'auto',
+    })
+
+
+
+
+    // 사이드 리모트 show/hide
+    var sc = window.scrollY;
+    $(window).scroll(function () {
+        var sc = $(document).scrollTop();
+        console.log(sc);
+        if (sc < 210) {
+            $('.side-remote').hide(500);
+        } else { $('.side-remote').show(500) }
+    })
+
+    // 사이드 리모트 이동
+    $('a.side-remote').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 600);
+    })
+
 
 });
 
