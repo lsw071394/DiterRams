71062f52c2a2a0f2f6657e24b59eaded4de3db6c
lsw071394 <sangwon0730@gmail.com>
2021-02-04 10:35:01 +0900

    .css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,14 @@
 // 모바일 페이지 로드시 상단 주소창 숨김
 window.addEventListener('load', function (e) {
-
+    $('html').scrollTop(0);
     setTimeout(scrollTo, 0, 0, 1);
 }, false);
 
 $(function () {
     console.log('jQuery Load')
-    // 광고창 사라지기전 body 리셋값
+    // body 리셋값
     $('body').css({ 'touch-action': 'none', 'overflow': 'hidden', 'background-color': '#141414' });
+    // $('.side-remote').hide();
 
     // 광고창 닫기 버튼
     $('.x').on('click', function () {
@@ -40,7 +41,21 @@ $(function () {
     // 햄버거 이벤트
     $(".ham").click(function (e) {
         e.preventDefault();
+        console.log('click')
         $(this).toggleClass("active")
+    });
+
+    // 배너 슬라이드 재생 정지
+    $(".pause").click(function (e) {
+        e.preventDefault();
+        console.log('click')
+        $(this).toggleClass("active")
+
+        if ($(this).hasClass('active')) {
+            swiper.autoplay.stop();
+        } else {
+            swiper.autoplay.start();
+        }
     });
 
     // 팝업창 닫기
@@ -62,6 +77,18 @@ $(function () {
         direction: 'horizontal',
         loop: false,
         slidesPerView: 'auto',
+    });
+
+    var swiper = new Swiper('.banner-area .swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 1500
+        },
+        paginationClickable: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
     });
 
     // 사이드 리모트 show/hide
